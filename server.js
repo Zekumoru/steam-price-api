cf03f670fe710d6cc5bd6f239baf60880f814751
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-03 21:48:02 +0100

    Add cors support

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 import router from './routes/router.js';
 
 const app = express();
+
+app.use(cors);
 
 app.use(bodyParser.json());
 app.use('/steam-price', router);
