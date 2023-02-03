eee19abcef54c9771090e770e1ffac612fc8bdc4
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-03 21:01:05 +0100

    Change route to steam-price

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ import router from './routes/router.js';
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/', router);
+app.use('/steam-price', router);
 
 app.listen(3000, () => console.log('Server has started'));
