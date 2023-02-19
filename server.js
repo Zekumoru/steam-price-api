43e5c7cc43ef409a69ecbab933d38aaa55f43eb6
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-19 02:08:59 +0100

    Install and use apicache

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
+import apicache from 'apicache';
 
 const app = express();
+const cache = apicache.middleware;
 
 app.use(cors());
 
+app.use(cache('2 days'));
 app.use(bodyParser.json());
 app.use('/steam-price', router);
 
