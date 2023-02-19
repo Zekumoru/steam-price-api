89058b144e58ff1005bb9be09775d414bad4d81f
Zekumoru Dragonhart <dev@zekumoru.com>
2023-02-19 02:39:06 +0100

    Add env vars and connect to db

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+import './load-env.js';
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
+import mongoose from 'mongoose';
+
+const PORT = process.env.PORT || 3010;
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.on('open', () => console.log('Connected to database'));
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/steam-price', router);
 
-app.listen(3000, () => console.log('Server has started'));
+app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
